95cc1666d21911c462b7f5c827e14b3c2e14daf9
Axel M. Siliezar <70248443+AxelSiliezar@users.noreply.github.com>
2021-03-30 19:13:14 -0700

    Initial commit

---
+++ HiddenGems/HiddenGems/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HiddenGems
+//
+//  Created by Axel Siliezar on 3/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
