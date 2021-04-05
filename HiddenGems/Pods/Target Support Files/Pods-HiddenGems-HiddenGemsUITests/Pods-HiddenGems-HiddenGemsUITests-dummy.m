f5ad38a9d55ba7aec579d9cc6b69ce045f37fbd5
ebarreto-mendoza <60796295+ebarreto-mendoza@users.noreply.github.com>
2021-04-05 13:04:34 -0700

    Podfile Created

---
+++ HiddenGems/Pods/Target Support Files/Pods-HiddenGems-HiddenGemsUITests/Pods-HiddenGems-HiddenGemsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HiddenGems_HiddenGemsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_HiddenGems_HiddenGemsUITests
+@end
---
+++ HiddenGems/Pods/Target Support Files/Pods-HiddenGems/Pods-HiddenGems-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HiddenGems : NSObject
+@end
+@implementation PodsDummy_Pods_HiddenGems
+@end
