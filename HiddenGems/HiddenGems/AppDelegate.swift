2aa6ca5877110f5ab0199eb0d065613fdb2aab2a
ebarreto-mendoza <60796295+ebarreto-mendoza@users.noreply.github.com>
2021-04-05 14:14:33 -0700

    Login/Signup Feature

--- HiddenGems/HiddenGems/AppDelegate.swift
+++ HiddenGems/HiddenGems/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // --- Copy this only
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "CE7Z8233ZA1DAHVEykcaHUEoY839fE0rLo4ANsOx"
+                $0.clientKey = "UOYePJUTgaoQsG5BWtTKyJZN6DS0Rs19zwCM5ueE"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        // --- end copy
+        
         return true
     }
 
