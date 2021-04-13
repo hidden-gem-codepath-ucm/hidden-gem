7fe7c4bac460617cf7c5cace707738503ec2e538
ebarreto-mendoza <60796295+ebarreto-mendoza@users.noreply.github.com>
2021-04-12 21:28:25 -0700

    Added backend to NewHiddenGemViewController to store information to Parse server

--- HiddenGems/HiddenGems/NewHiddenGemViewController.swift
+++ HiddenGems/HiddenGems/NewHiddenGemViewController.swift
@@ -6,8 +6,55 @@
 //
 
 import UIKit
+import CoreLocation
+import Parse
 
 class NewHiddenGemViewController: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var addressField: UITextField!
+    @IBOutlet weak var cityField: UITextField!
+    @IBOutlet weak var stateField: UITextField!
+    @IBOutlet weak var zipField: UITextField!
+    @IBOutlet weak var descriptionField: UITextView!
+    
+    
+    @IBAction func onAdd(_ sender: Any) {
+        let geocoder = CLGeocoder()
+        var addressString = addressField.text
+        addressString = (addressString! + ", " + cityField.text!) as String?
+        addressString = (addressString! + ", " + stateField.text!) as String?
+        addressString = (addressString! + ", " + zipField.text!) as String?
+        geocoder.geocodeAddressString(addressString!) { (placemarks, error) in
+                if error == nil {
+                    if let placemark = placemarks?[0] {
+                        let location = placemark.location!
+                        
+                        let hiddenGem = PFObject(className:"HiddenGems")
+                        hiddenGem["title"] = self.nameField.text
+                        hiddenGem["description"] = self.descriptionField.text
+                        hiddenGem["author"] = PFUser.current()
+                        hiddenGem["location"] = PFGeoPoint(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+                        hiddenGem.saveInBackground { (succeeded, error)  in
+                            if (succeeded) {
+                                print("Success in creating HiddenGem")
+                                self.dismiss(animated: true, completion: nil)
+                            } else {
+                                print("Error in creating HiddenGem: \(error)")
+                            }
+                        }
+                        return
+                    }
+                }
+                else{
+                    print("Error in converting address to GeoCode: \(error)")
+                }
+                    
+                
+            }
+    }
+    
+    
     
     @IBAction func cancelHiddenGem(_ sender: Any) {
         dismiss(animated: true, completion: nil)
